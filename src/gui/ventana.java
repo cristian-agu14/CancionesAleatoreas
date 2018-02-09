bf322155d7114e9804ec51c7a99b2d9abb561ca0
windowsdev <cristian.agu14@gmail.com>
2018-02-09 15:50:11 -0500

    Programa final version 1.0

--- src/gui/ventana.java
+++ src/gui/ventana.java
@@ -50,45 +50,48 @@ public class ventana extends JFrame {
 	public ventana() {
 		miCan = new Cancion();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 405);
+		setBounds(100, 100, 701, 405);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		JPanel panel = new JPanel();
-		panel.setBounds(23, 29, 411, 206);
+		panel.setBounds(23, 29, 652, 206);
 		contentPane.add(panel);
 		panel.setLayout(null);
 
 		JLabel labelNombre = new JLabel("Hola Amigos");
 		labelNombre.setForeground(Color.BLUE);
-		labelNombre.setFont(new Font("Comic Sans MS", Font.PLAIN, 30));
-		labelNombre.setBounds(20, 11, 381, 113);
+		labelNombre.setFont(new Font("Comic Sans MS", Font.PLAIN, 40));
+		labelNombre.setBounds(20, 11, 607, 113);
 		panel.add(labelNombre);
 
-		JLabel labelCantante = new JLabel("New label");
+		JLabel labelCantante = new JLabel("--");
 		labelCantante.setFont(new Font("Comic Sans MS", Font.PLAIN, 28));
-		labelCantante.setBounds(20, 147, 381, 48);
+		labelCantante.setBounds(82, 147, 510, 48);
 		panel.add(labelCantante);
 
 		JButton btnSiguiente = new JButton("SIGUIENTE");
 		btnSiguiente.setFont(new Font("Comic Sans MS", Font.PLAIN, 25));
 		btnSiguiente.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-
+				
+				labelNombre.setForeground(Color.BLUE);
+				labelCantante.setForeground(Color.black);
 				miCan = principal.buscarCancionPorId();
 				System.out.println("------>" + miCan.getNombre());
+				
 				if (miCan.isEstadoJ()) {
+					labelNombre.setForeground(Color.RED);
+					labelCantante.setForeground(Color.RED);
+				} 
 					labelNombre.setText(miCan.getNombre());
 					labelCantante.setText(miCan.getCantante());
-				} else {
-					labelNombre.setText(miCan.getNombre());
-					labelCantante.setText(miCan.getCantante());
-				}
+				
 			}
 		});
-		btnSiguiente.setBounds(115, 300, 234, 55);
+		btnSiguiente.setBounds(249, 300, 234, 55);
 		contentPane.add(btnSiguiente);
 
 	}
